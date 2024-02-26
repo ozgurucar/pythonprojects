c0fd6af4169531dea4e68891b439688a60c6febc
ozgurucar <ucar45ozgur@gmail.com>
2024-02-27 00:56:36 +0300

    bmi_calculator

---
+++ bmi_calculator.py
@@ -0,0 +1,16 @@
+height = int(input("Enter your height as centimeter"))
+weight = int(input("Enter your weight as kilogram"))
+height_in_meters = height / 100
+bmi = weight / (height_in_meters * height_in_meters)
+bmi = round(bmi, 2)
+print(f"Your bmi is {bmi}")
+if bmi < 18.5:
+    print("You are underweight")
+elif bmi >= 18.5 and bmi < 25:
+    print("You are in healthy range")
+elif bmi >= 25 and bmi < 30:
+    print("You are overweight")
+elif bmi >= 30 and bmi < 40:
+    print("You are obese")
+elif bmi >= 40:
+    print("You are in severe obesity section")
