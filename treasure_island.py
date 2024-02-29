0387057599c5dd85b801488ba8a3f8de77fb50e1
Ozgur Ucar <ucar.ozgur@outlook.com>
2024-03-01 02:31:03 +0300

    bugfix: treasure island

--- treasure_island.py
+++ treasure_island.py
@@ -9,7 +9,7 @@ print('''
 |                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
 |___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
           |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
- _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+ _________|___________| ;`-.o`"=._; ." ` '`."\\` . "-._ /_______________|_______
 |                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
 |___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
 ____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
@@ -25,9 +25,13 @@ print("Your mission is to find the treasure.")
 
 choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
 if choice1 == "left":
-    choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+    choice2 = input(
+        'You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type '
+        '"swim" to swim across. \n').lower()
     if choice2 == "wait":
-        choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+        choice3 = input(
+            "You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. "
+            "Which colour do you choose? \n").lower()
         if choice3 == "red":
             print("It's a room full of fire. Game Over!")
         elif choice3 == "yellow":
@@ -36,7 +40,10 @@ if choice1 == "left":
             print("You enter a room of beasts. Game Over.")
         else:
             print("You chose a door that doesn't exist. Game Over.")
+            exit()
     else:
         print("You get attacked by an angry trout. Game Over.")
+        exit()
 else:
     print("You fell into a hole. Game Over.")
+    exit()
