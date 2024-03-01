78e2a6ce06fd5b105689b640674d968b0faf25f6
Ozgur Ucar <ucar.ozgur@outlook.com>
2024-03-01 21:45:37 +0300

    bugfix endless loop of reeborg maze

--- reboorg_maze_solution.py
+++ reboorg_maze_solution.py
@@ -2,25 +2,17 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
-is_front_tried = False
+
+
+while front_is_clear():
+    move()
+turn_left()
+
 while not at_goal():
-    if front_is_clear() and right_is_clear():
-        if(is_front_tried == False):
-            move()
-            is_front_tried = True
-        else:
-            turn_right()
-            move()
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
     else:
-        if front_is_clear():
-            move()        
-        elif right_is_clear():
-            turn_right()
-            move()
-        else:
-            turn_left()
-    
-################################################################
-# WARNING: Do not change this comment.
-# Library Code is below.
-################################################################
+        turn_left()
